b747f3f002c8e21e4b369c9b94d55d827d55eac8
minseok980601 <minseok980601@gmail.com>
2022-04-07 00:43:36 +0900

    Kim Study Branch

--- StudyGit.java
+++ StudyGit.java
@@ -2,6 +2,7 @@ public class Git_Study {
 	public static void main(String[] args) {
 		System.out.println("Git Study");
 		System.out.println("Kim Push Git!!");
+		System.out.println("Branch Error?");
 	}
 }
 
